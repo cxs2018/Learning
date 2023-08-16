38f4150efbaa811a61f3c5734c724eaa5b5ae28c
cuixuesen <2821436626@qq.com>
2023-08-16 17:29:30 +0800

    feat: 学习工厂模式

--- interview/23.8.15JSSDK.js
+++ interview/23.8.15JSSDK.js
@@ -50,3 +50,232 @@ class Client {
     this.context.doSomething(data)
   }
 }
+
+// 抽象工厂接口声明了一组能返回不同抽象产品的方法，这些产品属于同一个系列，且在高层主题或概念上具有相关性。
+// 同系列的产品通常能相互搭配使用，系列产品可有多个变体，但不同变体的产品不能搭配使用
+class GUIFactory {
+  createButton() {
+
+  }
+  createCheckbox() {
+
+  }
+}
+
+// 具体工厂可生成属于统一变体的系列产品，工厂会确保其创建的产品能相互搭配使用。具体工厂方法签名回返回一个抽象产品，但在方法内部则会对具体产品进行实例化
+class WinFactory extends GUIFactory {
+  createButton() {
+    return new WinButton()
+  }
+  createCheckbox() {
+    return new WinCheckbox()
+  }
+}
+
+class MacFactory extends GUIFactory {
+  createButton() {
+    return new MacButton()
+  }
+  createCheckbox() {
+    return new MacCheckbox()
+  }
+}
+
+// 系列产品中的特定产品必须有一个基础接口，所有产品变体都必须实现这个接口
+class Button {
+  paint() {}
+}
+
+// 具体产品有相应的具体工厂创建
+class WinButton extends Button {
+  paint() {
+    // windows风格按钮创建过程
+    console.log("绘制windows风格按钮")
+  }
+}
+
+class MacButton extends Button {
+  paint() {
+    // mac风格按钮创建过程
+    console.log("绘制mac风格按钮")
+  }
+}
+
+// 系列产品中的特定产品必须有一个基础接口，所有产品变体都必须实现这个接口
+class Checkbox {
+  paint() {}
+}
+
+// 具体产品有相应的具体工厂创建
+class WinCheckbox extends Checkbox {
+  paint() {
+    // windows风格复选框创建过程
+    console.log("绘制windows风格复选框")
+  }
+}
+
+class MacCheckbox extends Checkbox {
+  paint() {
+    // mac风格复选框创建过程
+    console.log("绘制mac风格复选框")
+  }
+}
+
+// 客户端代码仅通过抽象类型（GUIFactory、Button和Checkbox）使用工厂和产品，无需修改任何工厂或产品子类就能传递给客户端代码
+class Application {
+  constructor(factory) {
+    this.factory = factory
+    this.button = null
+  }
+
+  createUI() {
+    this.button = this.factory.createButton()
+    this.checkbox = this.factory.createCheckbox()
+  }
+
+  paint() {
+    this.button.paint()
+    this.checkbox.paint()
+  }
+}
+
+class ApplicationConfigurator {
+  constructor() {
+    let config = {OS: "Mac"}
+    if (config.OS === 'Windows') {
+      this.factory = new WinFactory()
+    } else if (config.OS === "Mac"){
+      this.factory = new MacFactory()
+    } else {
+      throw new Error("错误！未知的操作系统")
+    }
+
+    let app = new Application(this.factory)
+
+    app.createUI()
+    app.paint()
+  }
+}
+
+new ApplicationConfigurator()
+
+// 简单工厂模式
+class Human {
+  printColor() {}
+  talk() {}
+}
+
+class BlackHuman extends Human {
+  printColor() {
+    console.log("黑种人")
+  }
+  talk() {
+    console.log("黑种人说话了")
+  }
+}
+
+class WhiteHuman extends Human {
+  printColor() {
+    console.log("白种人")
+  }
+  talk() {
+    console.log("白种人说话了")
+  }
+}
+
+class YellowHuman extends Human {
+  printColor() {
+    console.log("黄种人")
+  }
+  talk() {
+    console.log("黄种人说话了")
+  }
+}
+
+class HumanFactory {
+  createHuman(type) {
+    if (type === 'black') {
+      return new BlackHuman()
+    } else if (type === 'white') {
+      return new WhiteHuman()
+    } else if (type === 'yellow') {
+      return new YellowHuman()
+    } else {
+      return null
+    }
+  }
+}
+
+function NvWa() {
+  let factory = new HumanFactory()
+
+  console.log("开始创造黑种人......")
+  let blackHuman = factory.createHuman("black");
+  blackHuman.printColor()
+  blackHuman.talk()
+
+  console.log("开始创造白种人......")
+  let whiteHuman = factory.createHuman("white");
+  whiteHuman.printColor()
+  whiteHuman.talk()
+
+  console.log("开始创造黄种人......")
+  let yellowHuman = factory.createHuman("yellow");
+  yellowHuman.printColor()
+  yellowHuman.talk()
+}
+
+NvWa()
+
+console.log("--------------------------------")
+
+// 工厂方法模式
+class AbstractHumanFactory {
+  createHuman() {}
+}
+
+class BlackHumanFactory extends AbstractHumanFactory {
+  createHuman() {
+    return new BlackHuman()
+  }
+}
+
+class WhiteHumanFactory extends AbstractHumanFactory {
+  createHuman() {
+    return new WhiteHuman()
+  }
+}
+
+class YellowHumanFactory extends AbstractHumanFactory {
+  createHuman() {
+    return new YellowHuman()
+  }
+}
+
+function NvWaFactoryMethod() {
+  let factory = null;
+
+  console.log("开始创造黑种人......")
+  factory = new BlackHumanFactory()
+  let blackHuman = factory.createHuman()
+  blackHuman.printColor()
+  blackHuman.talk()
+
+  console.log("开始创造白种人......")
+  factory = new WhiteHumanFactory()
+  let whiteHuman = factory.createHuman()
+  whiteHuman.printColor()
+  whiteHuman.talk()
+
+  console.log("开始创造黄种人......")
+  factory = new YellowHumanFactory()
+  let yellowHuman = factory.createHuman()
+  yellowHuman.printColor()
+  yellowHuman.talk()
+}
+
+NvWaFactoryMethod()
+
+(function() {
+  
+})()
