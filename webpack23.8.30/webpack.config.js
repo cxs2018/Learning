51a3e790bb6d1f4e9a5832c03541a48dca2a4f13
cuixuesen <2821436626@qq.com>
2023-08-31 17:41:55 +0800

    feat: 学习 Assets module

--- webpack23.8.30/webpack.config.js
+++ webpack23.8.30/webpack.config.js
@@ -1,16 +1,18 @@
 const path = require("path")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   // webpack寻找相对路径的文件时会以context为目录，默认为执行启动webpack时所在的当前工作目录
   context: path.resolve(__dirname, ''),
   // JS 执行入口文件
-  entry: "./src/main.js",
+  // entry: "./src/main.js",
+  entry: './learnImgBuild/index.js',
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
     filename: "bundle.js",
     // 输出文件都放到 dist 目录下
-    path: path.resolve(__dirname, "./dist")
+    path: path.resolve(__dirname, "./dist2")
   },
   mode: "development",
   // loader 处理非JS文件，如css、图片等资源
@@ -21,6 +23,27 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
         // use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        type: "asset",
+        //解析
+        parser: {
+          //转base64的条件
+          dataUrlCondition: {
+            maxSize: 25 * 1024, // 25kb
+          }
+        },
+        generator: {
+          //与output.assetModuleFilename是相同的,这个写法引入的时候也会添加好这个路径
+          filename: 'img/[name].[hash:6][ext]',
+          //打包后对资源的引入，文件命名已经有/img了
+          publicPath: './'
+        },
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
       }
     ]
   },
@@ -30,4 +53,10 @@ module.exports = {
   //     filename: `[name]_[contenthash:8].css`
   //   })
   // ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './learnImgBuild/index.html',
+      inject: 'body'
+    })
+  ]
 }
