0246ac7d3315d7d3627ddded2815226138db9efc
cuixuesen <2821436626@qq.com>
2023-09-08 14:06:18 +0800

    feat: 学习split chunk

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,40 +1,71 @@
-import _ from 'lodash'
-import "./style.css"
-/**
- * 遇到 import Icon from './icon.png'，图片将被复制到 output 目录，并且，Icon 变量将包含该图像在处理后的最终 url
- */
-import Icon from './icon.png'
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
+// import _ from 'lodash'
+// import "./style.css"
+// /**
+//  * 遇到 import Icon from './icon.png'，图片将被复制到 output 目录，并且，Icon 变量将包含该图像在处理后的最终 url
+//  */
+// import Icon from './icon.png'
+// import Data from './data.xml';
+// import Notes from './data.csv';
+// import toml from './data.toml';
+// import yaml from './data.yaml';
+// import json from './data.json5';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
+// console.log(toml.title); // output `TOML Example`
+// console.log(toml.owner.name); // output `Tom Preston-Werner`
 
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
+// console.log(yaml.title); // output `YAML Example`
+// console.log(yaml.owner.name); // output `Tom Preston-Werner`
 
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+// console.log(json.title); // output `JSON5 Example`
+// console.log(json.owner.name); // output `Tom Preston-Werner`
 
+
+// function component() {
+//   const element = document.createElement("div")
+
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   element.classList.add("hello")
+
+//   const myIcon = new Image()
+//   myIcon.src = Icon;
+
+//   element.appendChild(myIcon)
+
+//   console.log(Data);
+//   console.log(Notes);
+
+//   return element;
+// }
+
+// document.body.appendChild(component())
+
+// 动态导入
+// function getComponent() {
+//   return import("lodash").then(({ default: _ }) => {
+//     const element = document.createElement("div")
+
+//     element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+//     return element
+//   }).catch(error => "Error")
+// }
+
+// getComponent().then((component) => {
+//   document.body.appendChild(component)
+// })
+
+// 缓存
+import _ from 'lodash';
+// import Print from './print';
 
 function component() {
-  const element = document.createElement("div")
+  const element = document.createElement('div');
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello")
-
-  const myIcon = new Image()
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon)
-
-  console.log(Data);
-  console.log(Notes);
+  // lodash 现在使用 import 引入
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.onclick = Print.bind(null, 'Hello webpack!');
 
   return element;
 }
 
-document.body.appendChild(component())
+document.body.appendChild(component());
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,56 +2,87 @@ const path = require("path")
 const toml = require('toml');
 const yaml = require('yamljs');
 const json5 = require('json5');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
+  mode: 'development',
+  entry: {
+    main: {
+      import: "./src/index.js",
+      // dependOn: "shared"
+    },
+    // another: {
+    //   import: "./src/another-module.js",
+    // dependOn: "shared"
+    // },
+    // shared: "lodash"
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i,
+  //       use: ["style-loader", "css-loader"]
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  //       type: "asset/resource"
+  //     },
+  //     {
+  //       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+  //       type: 'asset/resource',
+  //     },
+  //     {
+  //       test: /\.(csv|tsv)$/i,
+  //       use: ['csv-loader'],
+  //     },
+  //     {
+  //       test: /\.xml$/i,
+  //       use: ['xml-loader'],
+  //     },
+  //     {
+  //       test: /\.toml$/i,
+  //       type: 'json',
+  //       parser: {
+  //         parse: toml.parse,
+  //       },
+  //     },
+  //     {
+  //       test: /\.yaml$/i,
+  //       type: 'json',
+  //       parser: {
+  //         parse: yaml.parse,
+  //       },
+  //     },
+  //     {
+  //       test: /\.json5$/i,
+  //       type: 'json',
+  //       parser: {
+  //         parse: json5.parse,
+  //       },
+  //     },
+  //   ]
+  // },
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single', // 将 runtime 代码拆分为一个单独的chunk，设置为 single 为所有chunk创建一个 runtime bundle
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource"
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ]
-  }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "caching"
+    })
+  ]
 }
