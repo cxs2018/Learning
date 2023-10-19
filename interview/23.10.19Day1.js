0de3730d10199bb0018b8d28e1fc494bed9d52e8
cuixuesen <2821436626@qq.com>
2023-10-19 22:30:55 +0800

    feat: 实例属性与原型属性

--- interview/23.10.19Day1.js
+++ interview/23.10.19Day1.js
@@ -20,10 +20,34 @@ function getName() {
 
 debugger;
 // 请写出以下输出结果：
-Foo.getName(); // 2
-getName(); // 4
-Foo().getName(); // 1 Foo 内部的getName没有用var修饰，会给全局变量 getName 赋值
-getName(); // 1
-new Foo.getName(); // 2
-new Foo().getName(); // 3
-new new Foo().getName(); // 3
+// Foo.getName(); // 2
+// getName(); // 4
+// Foo().getName(); // 1 Foo 内部的getName没有用var修饰，会给全局变量 getName 赋值
+// getName(); // 1
+// new Foo.getName(); // 2
+// new Foo().getName(); // 3
+// new new Foo().getName(); // 3
+
+/**
+ * new 运算符的优先级
+ * new带参数，如 new Foo()、new Foo(1,2)，带参数是指后面有括号可以传递参数 和 . 属性访问运算符优先级一样，从左往后运算
+ * new 不带参数，如 new Foo，优先级比 . 属性访问低
+ * new new 从右往左执行
+ */
+
+function Car() {}
+car1 = new Car();
+car2 = new Car();
+
+console.log(car1.color); // undefined
+
+Car.prototype.color = { a: "A" };
+console.log(car1.color); // original color
+
+car1.color.B = "B";
+console.log(car1.color); // black
+
+console.log(car1.__proto__.color); //original color
+console.log(car2.__proto__.color); //original color
+console.log(car1.color); // black
+console.log(car2.color); // original color
