e401d6fb778f9a31e6536977abdedd205404aa2b
cuixuesen <2821436626@qq.com>
2023-12-12 14:52:13 +0800

    feat: 防抖节流

--- interview/23.8.24防抖节流.js
+++ interview/23.8.24防抖节流.js
@@ -4,35 +4,52 @@
 function debounce(fn, delay = 200, leading) {
   let timeout;
   return function () {
-    timeout ? clearTimeout(timeout) : leading ? fn.call(this, ...arguments) : null
-    timeout = setTimeout(fn.bind(this), delay, ...arguments)
-  }
+    timeout
+      ? clearTimeout(timeout)
+      : leading
+      ? fn.call(this, ...arguments)
+      : null;
+    timeout = setTimeout(fn.bind(this), delay, ...arguments);
+  };
 }
 
-const handlerChange = debounce(function () { console.log('更新触发了') }, 1000, true)
+const handlerChange = debounce(
+  function () {
+    console.log("更新触发了");
+  },
+  1000,
+  true,
+);
 
 // 绑定监听
-document.querySelector("input").addEventListener('input', handlerChange);
+document.querySelector("input").addEventListener("input", handlerChange);
 
-function throttle(fn, threshhold = 200, leading) {
+function throttle(fn, threshold = 200, leading) {
   let timeout;
   let start = new Date();
   return function () {
     const current = new Date();
-    timeout ? clearTimeout(timeout) : leading ? fn.call(this, ...arguments) : null
-    if (current - start >= threshhold) {
-      fn.call(this, ...arguments)
-      start = current
+    timeout
+      ? clearTimeout(timeout)
+      : leading
+      ? fn.call(this, ...arguments)
+      : null;
+    if (current - start >= threshold) {
+      fn.call(this, ...arguments);
+      start = current;
     } else {
-      timeout = setTimeout(fn.bind(this), threshhold, ...arguments)
+      timeout = setTimeout(fn.bind(this), threshold, ...arguments);
     }
-  }
+  };
 }
 
-
-let handleMouseMove = throttle(function (e) {
-  console.log(e.pageX, e.pageY);
-}, 2000, true)
+let handleMouseMove = throttle(
+  function (e) {
+    console.log(e.pageX, e.pageY);
+  },
+  2000,
+  true,
+);
 
 // 绑定监听
-document.querySelector("body").addEventListener('mousemove', handleMouseMove);
+document.querySelector("body").addEventListener("mousemove", handleMouseMove);
--- interview/技术体系/js/index.md
+++ interview/技术体系/js/index.md
@@ -2,6 +2,64 @@
 
 ### 防抖与节流
 
+WHAT？
+本质上是优化高频率执行代码的一种手段，如浏览器的 resize、scroll、keypress、mousemove 等事件在触发时，
+会不断地调用绑定在事件上的回调函数，极大地浪费资源，降低前端性能。对此我们可以采用防抖和节流的方式来限制调用次数，降低调用频率。
+
+防抖：当事件触发n秒后执行函数，如果在n秒内再次触发，重新计时
+节流：当多次执行某一动作，每隔一段时间，只执行一次函数
+
+为什么叫防抖？防止抖动，以免把一次事件误认为是多次。单位时间内事件触发会被重置，避免事件被误伤触发多次。代码实现重在清零 clearTimeout。防抖可以比做等电梯，只要有一个人进来，就需要再
+等一会儿。业务场景有：
+1. 登录、发短信等按钮避免用户点击太快，以至于发送了多次请求，需要防抖；
+2. 调整浏览器窗口大小时，避免resize事件处理函数执行次数过于频繁引起业务卡顿（如函数处理函数有比较重的计算逻辑）；
+3. 文本编辑器实时保存，当无任何更改操作一秒后进行保存。
+
+为什么叫节流？控制流量、节省流量、限制流量，即控制事件发生的频率，如1s内只执行一次。业务场景有：
+1. 浏览器 scroll 事件，每隔一秒重新计算一次位置信息；
+2. 视频播放事件，每一秒计算一次进度信息；
+3. input款实时搜索并发送请求展示下拉列表，每隔一秒发送一次请求（也可做防抖）。
+
+HOW？
+```js
+/**
+ * 节流
+ * @param fn 要节流的函数
+ * @param threshold 节流频率
+ * @param leading 是否要立即执行
+ * @returns {(function(): void)|*}
+ */
+function throttle(fn, threshold= 200, leading) {
+  let timer;
+  let start = new Date();
+  return function () {
+    const current = new Date();
+    timer ? clearTimeout(timer) : leading ? fn.call(this, ...arguments) : null;
+    if (current - start >= threshold) {
+      fn.call(this, ...arguments);
+      start = current;
+    } else {
+      timer = setTimeout(fn.bind(this), threshold, ...arguments);
+    }
+  }
+}
+
+/**
+ * 防抖
+ * @param fn 要防抖的函数
+ * @param delay 延迟的事件
+ * @param leading 是否要立即执行
+ * @returns {(function(): void)|*}
+ */
+function debounce(fn, delay = 200, leading) {
+  let timer;
+  return function () {
+    timer ? clearTimeout(timer) : leading ? fn.call(this, ...arguments) : null;
+    timer = setTimeout(fn.bind(this), delay, ...arguments);
+  }
+}
+```
+
 ### JS的各种数据类型
 
 ### Map和WeakMap
