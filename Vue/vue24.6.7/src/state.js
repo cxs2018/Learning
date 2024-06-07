5bc0990f3fd6e8110bcd23a45d32858ddb3f1a1b
cuixuesen <2821436626@qq.com>
2024-06-07 18:40:49 +0800

    feat: vm代理

--- Vue/vue24.6.7/src/state.js
+++ Vue/vue24.6.7/src/state.js
@@ -17,9 +17,24 @@ export function initState(vm) {
   // }
 }
 
+function proxy(vm, source, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[source][key];
+    },
+    set(v) {
+      vm[source][key] = v;
+    },
+  });
+}
+
 function initData(vm) {
   let data = vm.$options.data;
   // vue2中会讲data中的所有数据进行数据劫持 Object.defineProperty
   data = vm._data = isFunction(data) ? data.call(vm) : data;
+
+  for (const key in data) {
+    proxy(vm, "_data", key);
+  }
   observer(data);
 }
