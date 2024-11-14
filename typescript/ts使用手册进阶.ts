93af246eaa89da63171be75fd4ce3d55e548f080
cuixuesen <2821436626@qq.com>
2024-11-14 19:59:44 +0800

    feat: 交叉类型

---
+++ typescript/ts使用手册进阶.ts
@@ -0,0 +1,51 @@
+export {};
+// 交叉类型 Intersection Types
+function extend<First extends object, Second extends object>(
+  first: First,
+  second: Second,
+): First & Second {
+  const result: Partial<First & Second> = {};
+  for (const prop in first) {
+    if (first.hasOwnProperty(prop)) {
+      (result as First)[prop] = first[prop];
+    }
+  }
+  for (const prop in second) {
+    if (second.hasOwnProperty(prop)) {
+      (result as Second)[prop] = second[prop];
+    }
+  }
+  return result as First & Second;
+}
+class Person {
+  constructor(public name: string) {}
+}
+interface Loggable {
+  log(name: string): void;
+}
+class ConsoleLogger implements Loggable {
+  log(name: string) {
+    console.log(`Hello, I'm ${name}.`);
+  }
+}
+const jim = extend(new Person("Jim"), ConsoleLogger.prototype);
+jim.log(jim.name);
+type FunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never;
+}[keyof T];
+type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
+type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K;
+}[keyof T];
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+interface Part {
+  id: number;
+  name: string;
+  subparts: Part[];
+  updatePart(newName: string): void;
+}
+
+type T40 = FunctionPropertyNames<Part>; // "updatePart"
+type T41 = NonFunctionPropertyNames<Part>; // "id" | "name" | "subparts"
+type T42 = FunctionProperties<Part>; // { updatePart(newName: string): void }
+type T43 = NonFunctionProperties<Part>; // { id: number, name: string, subparts: Part[] }
