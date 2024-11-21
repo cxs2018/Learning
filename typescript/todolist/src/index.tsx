9f9d309201d13bf95f35fb6c588af5eeadaefa72
cuixuesen <2821436626@qq.com>
2024-11-21 11:10:31 +0800

    feat: ts connected-react-router run

--- typescript/todolist/src/index.tsx
+++ typescript/todolist/src/index.tsx
@@ -4,27 +4,29 @@ import Counter from "./components/Counter";
 import Todos from "./components/Todos";
 import store from "@/store";
 import { Provider } from "react-redux";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, BrowserRouter } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import history from "./history";
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <React.Fragment>
-        <ul>
-          <li>
-            <Link to={"/counter/counterName"}>counter</Link>
-          </li>
-          <li>
-            <Link to={{ pathname: "/todos", state: { name: "todosName" } }}>
-              todos
-            </Link>
-          </li>
-        </ul>
-        <Route path={"/counter/:name"} component={Counter} />
-        <Route path={"/todos"} component={Todos} />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <ul>
+            <li>
+              <Link to={"/counter/counterName"}>counter</Link>
+            </li>
+            <li>
+              <Link to={{ pathname: "/todos", state: { name: "todosName" } }}>
+                todos
+              </Link>
+            </li>
+          </ul>
+          <Route path={"/counter/:name"} component={Counter} />
+          <Route path={"/todos"} component={Todos} />
+        </React.Fragment>
+      </BrowserRouter>
     </ConnectedRouter>
   </Provider>,
   document.getElementById("root"),
